86e031af916eb841f95297d29b1301fab4134464
Robert Patterson <robert@robertgpatterson.com>
2021-09-04 10:04:17 -0500

    added test for FCStaffNamePostion

---
+++ classtests/jwluatest_fcstaffnameposition.lua
@@ -0,0 +1,27 @@
+function FCStaffNamePosition_PropertyTests(classname, prefs)
+   NumberPropertyTest(prefs, classname, "Alignment", {finale.TEXTHORIZALIGN_LEFT, finale.TEXTHORIZALIGN_RIGHT, finale.TEXTHORIZALIGN_CENTER})
+   BoolPropertyTest(prefs, classname, "ExpandSingleWord")
+   NumberPropertyTest(prefs, classname, "HorizontalOffset", {-144, 0, 144})
+   NumberPropertyTest(prefs, classname, "Justification", {finale.TEXTJUSTIFY_LEFT, finale.TEXTJUSTIFY_RIGHT, finale.TEXTJUSTIFY_CENTER,
+      finale.TEXTJUSTIFY_FULL, finale.TEXTJUSTIFY_FORCED_FULL})
+   BoolPropertyTest(prefs, classname, "UsePositioning")
+   NumberPropertyTest(prefs, classname, "VerticalOffset", {-144, 0, 144})
+end
+
+-- Call:
+local staves = finale.FCStaves()
+staves:LoadAll()
+for staff in each(staves) do
+   -- The first staff in the test document must have Full and Abbreviated name positions overridden
+   -- Otherwise these tests generate spurious errors
+
+   local namepos_full = staff:GetFullNamePosition()
+   AssureTrue(namepos_full ~= nil, "FCStaffNamePosition - LoadFull")
+   FCStaffNamePosition_PropertyTests("FCStaffNamePosition", namepos_full)
+
+   local namepos_abrv = staff:GetAbbreviatedNamePosition()
+   AssureTrue(namepos_abrv ~= nil, "FCStaffNamePosition - Abbreviated")
+   FCStaffNamePosition_PropertyTests("FCStaffNamePosition", namepos_abrv)
+
+   break -- only need to test one staff
+end
