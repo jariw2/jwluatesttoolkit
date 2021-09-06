21300b09e88075cc596552a7b65990bb9fee719d
Robert Patterson <robert@robertgpatterson.com>
2021-09-06 11:17:42 -0500

    add layer prefs

---
+++ classtests/jwluatest_fclayerprefs.lua
@@ -0,0 +1,20 @@
+function FCLayerPrefs_PropertyTests(prefs)
+   BoolPropertyTest(prefs, "FCLayerPrefs", "FreezeStemsUp")
+   BoolPropertyTest(prefs, "FCLayerPrefs", "FreezeTiesSameDirection")
+   BoolPropertyTest(prefs, "FCLayerPrefs", "HideWhenInactive")
+   BoolPropertyTest(prefs, "FCLayerPrefs", "IgnoreHiddenLayers")
+   BoolPropertyTest(prefs, "FCLayerPrefs", "IgnoreHiddenNotes")
+   NumberPropertyTest_RO(prefs, "FCLayerPrefs", "LayerIndex")
+   NumberPropertyTest_RO(prefs, "FCLayerPrefs", "LayerNumber")
+   BoolPropertyTest(prefs, "FCLayerPrefs", "Playback")
+   NumberPropertyTest(prefs, "FCLayerPrefs", "RestOffset", {-2, 0, 3})
+   BoolPropertyTest(prefs, "FCLayerPrefs", "Spacing")
+   BoolPropertyTest(prefs, "FCLayerPrefs", "UseFreezeStemsTies")
+   BoolPropertyTest(prefs, "FCLayerPrefs", "UseRestOffset")
+   BoolPropertyTest(prefs, "FCLayerPrefs", "UseRestOffsetInMultiple")
+end
+
+-- Call:
+local prefs = finale.FCLayerPrefs()
+AssureTrue(prefs:Load(1))
+FCLayerPrefs_PropertyTests(prefs)
