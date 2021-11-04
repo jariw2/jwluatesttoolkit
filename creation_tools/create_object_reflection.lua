65bbaabce273eb2a07873ed6937191d78d002a69
Robert Patterson <robert@robertgpatterson.com>
2021-11-04 16:43:20 -0500

    add 'AddClass' calls to create_object_reflection.lua

--- creation_tools/create_object_reflection.lua
+++ creation_tools/create_object_reflection.lua
@@ -3,7 +3,7 @@ function plugindef()
    -- are both reserved for the plug-in definition.
    finaleplugin.RequireDocument = false -- ignored by jw lua 0.54
    finaleplugin.NoStore = true
-   finaleplugin.Author = "Jari Williamsson"
+   finaleplugin.Author = "Robert Patterson"
    finaleplugin.CategoryTags = "Debug, Development, Diagnose, UI"
    return "Create Object Reflection", "Create Object Reflection", "Reflect selected class(es) to the clipboard in refl-cpp format."
 end
@@ -13,16 +13,35 @@ if finenv.MinorVersion > 54 then -- if new lua
 end
 
 local refl_output = ""
-local classname = "FCEntryMetrics" -- change this to the class you want, if the dialog isn't available (i.e. RGP Lua)
+local classname = ""
 
 -- Show dialog
-local dialog = nil
-if finenv.MinorVersion <= 54 then -- old jw lua version
-    dialog = finenv.UserValueInput()
-    dialog.Title = "Class Name To Reflect"
-    dialog:SetTypes("String")
-    dialog:SetDescriptions("Class name (empty to list all):")
+local dialog = function()
+    local str = finale.FCString()
+    local dialog = finale.FCCustomWindow()
+    str.LuaString = "Class Name To Reflect"
+    dialog:SetTitle(str)
+    local current_y = 0
+    local x_increment = 105
+    -- number of steps
+    local static = dialog:CreateStatic(0, current_y+2)
+    str.LuaString = "Class name (empty to list all):"
+    static:SetText(str)
+    static:SetWidth(200)
+    current_y = current_y + 20
+    local class_name = dialog:CreateEdit(0, current_y)
+    class_name:SetWidth(200)
+    -- ok/cancel
+    dialog:CreateOkButton()
+    dialog:CreateCancelButton()
+    if finale.EXECMODAL_OK == dialog:ExecuteModal(nil) then
+        local classname_entered = finale.FCString()
+        class_name:GetText(classname_entered)
+        return true, classname_entered
+    end
+    return false
 end
+
 
 local num_methods = 0
 local num_classes = 0
@@ -37,9 +56,34 @@ end
 
 function __prop_table(classtable, key)
     if finenv.MinorVersion <= 54 then
-        return classtable.__class[key]
+        if classtable.__class then
+            return classtable.__class[key]
+        end
     end
     return classtable[key]
+end
+
+local get_parent_name = function(class, classname)
+    if finenv.MinorVersion <= 54 then -- old jw lua
+        classt = class.__class
+        if classt and classname ~= "__FCBase" then
+            classtp = classt["__parent"] -- this line will crash Finale (in jw lua 0.54) if "__parent" doesn't exist, so we excluded "__FCBase" above, the only class without a parent
+            if classtp and type(classtp) == "table" then            
+                for k, v in pairs(_G.finale) do
+                    if type(v) == "table" then
+                        if v.__class and v.__class == classtp then
+                            return tostring(k)
+                        end
+                    end                
+                end
+            end
+        end
+    else
+        for k, v in pairs(class.__parent) do
+            return tostring(k)  -- post 0.54 the v is just a dummy value, and the key is the classname of the parent
+        end
+    end
+    return nil
 end
 
 function CountMethods(classtable)
@@ -57,7 +101,12 @@ function CountMethods(classtable)
     return retval
 end
 
+classes_found = {}
+
 function ProcessClass(classname, classtable)
+    if finenv.MinorVersion <= 54 and refl_output == "" then
+        refl_output = refl_output .. "// This script cannot detect static functions when run with OG JW Lua (0.54 and earlier).\n\n"
+    end
     local proptable = {}
     local __propget = __prop_table(classtable,"__propget")
     if __propget then
@@ -106,7 +155,7 @@ function ProcessClass(classname, classtable)
             else
                 refl_output = refl_output .. "   REFL_FUNC(" .. kstr
             end
-            if kstr:find("Create") == 1 then
+            if kstr:find("Create") == 1 and classname ~= "FCCustomWindow" then
                 refl_output = refl_output .. '_GC, special_name("' .. kstr .. '")'
             end
             if nil ~= proptable[k] then
@@ -129,6 +178,7 @@ function ProcessClass(classname, classtable)
         refl_output = refl_output .. "REFL_END\n"
     end
     num_classes = num_classes + 1
+    classes_found[classname] = {parent_name = get_parent_name(classtable, classname), has_constructor = (nil ~= classtable.__call)}
     for k, v in pairs(proptable) do
         if not v then
             print ("WARNING: Property getter or setter not found in functions: " .. classname .. "." .. k)
@@ -137,9 +187,9 @@ function ProcessClass(classname, classtable)
 end
 
 if dialog ~= nil then
-    local returnvalues = dialog:Execute()
-    if returnvalues == nil then return end
-    classname = returnvalues[1]
+    local success, str = dialog()
+    if not success then return end
+    classname = str.LuaString
 end
 
 for k, v in pairs(_G.finale) do
@@ -150,6 +200,23 @@ for k, v in pairs(_G.finale) do
                 ProcessClass(k, v)
             end
         end
+    end
+end
+
+if num_classes > 0 then
+    refl_output = refl_output .. "\n// Be sure to check the constructor signatures.\n"
+    refl_output = refl_output .. "// This script can detect the presence of constructors but not their signatures.\n"
+    refl_output = refl_output .. "// (The c++ compiler will refuse to compile if a constructor signature is wrong.)\n\n"
+    for k, v in pairs(classes_found) do
+        if v.parent_name then
+            refl_output = refl_output .. "AddDerivedClass<" .. k .. ", " .. v.parent_name
+        else
+            refl_output = refl_output .. "AddClass<" .. k
+        end
+        if v.has_constructor then
+            refl_output = refl_output .. ", void(*) (void)" -- the no-parameters constructor used by the vast majority of FC classes
+        end
+        refl_output = refl_output .. ">(nameSpace, l);\n"
     end
 end
 
