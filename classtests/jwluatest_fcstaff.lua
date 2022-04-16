a4ebc494f620a92ffeff582a715556659bd85301
Robert Patterson <robert@robertgpatterson.com>
2022-04-16 14:27:44 -0500

    test additional emum value

--- classtests/jwluatest_fcstaff.lua
+++ classtests/jwluatest_fcstaff.lua
@@ -8,7 +8,7 @@ function FCStaff_PropertyTests(staff)
         finale.ALTSTAFF_BLANKNOTATION } )
    NumberPropertyTest(staff, "FCStaff", "AutoNumberingStyle", { finale.STAFFAUTONUMBER_ARABIC_SUFFIX,
         finale.STAFFAUTONUMBER_ROMAN_SUFFIX, finale.STAFFAUTONUMBER_ORDINAL_PREFIX,
-        finale.STAFFAUTONUMBER_ALPHA_SUFFIX } )
+        finale.STAFFAUTONUMBER_ALPHA_SUFFIX, finale.STAFFAUTONUMBER_ARABIC_PREFIX } )
    NumberPropertyTest(staff, "FCStaff", "BottomBarlineOffset", { -299, 0, 299 } )
    BoolPropertyTest(staff, "FCStaff", "BreakBarlines")
    BoolPropertyTest(staff, "FCStaff", "BreakRepeatBarlines")
