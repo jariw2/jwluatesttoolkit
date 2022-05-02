78cd4f29a69b050ac86649fada9547741a80ec06
Robert Patterson <robert@robertgpatterson.com>
2022-05-02 09:35:18 -0500

    add remaining property tests for FCTieMod.

--- constanttests/consttest_fctiemod.lua
+++ constanttests/consttest_fctiemod.lua
@@ -6,3 +6,13 @@ end
 
 -- Test the constants:
 TestConstants_TIEMODDIR_()
+
+
+function TestConstants_TIEMODSEL_()
+   NumberConstantTest(finale.TIEMODSEL_DEFAULT, "TIEMODSEL_DEFAULT", 0)
+   NumberConstantTest(finale.TIEMODSEL_ON, "TIEMODSEL_ON", 1)
+   NumberConstantTest(finale.TIEMODSEL_OFF, "TIEMODSEL_OFF", 2)
+end
+
+-- Test the constants:
+TestConstants_TIEMODSEL_()
