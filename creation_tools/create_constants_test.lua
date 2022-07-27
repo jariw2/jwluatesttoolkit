88cc014c8c8937b6818a6028e6cae36519549855
Robert Patterson <robert@robertgpatterson.com>
2022-07-27 15:47:30 -0500

    fix up line endings

--- creation_tools/create_constants_test.lua
+++ creation_tools/create_constants_test.lua
@@ -8,7 +8,6 @@ function plugindef()
    return "Create Finale Lua Constants Test", "Create constants test", "Creates a test for the constants available to Finale Lua."
 end
 
-
 local pattern = "UNDOSTATE_" -- edit as needed in RGP Lua
 
 local get_finale_propget = function()
@@ -27,15 +26,11 @@ if finenv.IsRGPLua then
     require('mobdebug').start()
 end
 if not finenv.IsRGPLua then
-
     -- Show dialog
-
     local dialog = finenv.UserValueInput()
     dialog.Title = "Test JW Lua Constants"
     dialog:SetTypes("String")
     dialog:SetDescriptions("Search string (empty to list all):")
-
-
 
     local returnvalues = dialog:Execute()
     if returnvalues == nil then return end
@@ -45,93 +40,51 @@ if not finenv.IsRGPLua then
         pattern = returnvalues[1]
     end
 end
-
 local result = {}
-
 -- Search for constants
-
 local found = false
 local propget = get_finale_propget()
-
 if type(propget) == "table" then
     for k1, v1 in pairs(propget) do
-
         if string.find(k1, pattern) or pattern == "" then
-
             table.insert(result, k1)                
-
             found = true
-
         end
-
     end
-
 end
-
 if not found then
-
     print ("No constants found")
-
     return
-
 end
-
 -- Create sorted result as a test:
-
 local ResultString = ""
-
 ResultString = ResultString .. "function TestConstants_" .. pattern .. "()\n"
-
-
 
 table.sort(result)
 
-
-
 for k, v in pairs(result) do
-
     local value = _G.finale[v]
-
     local prefix = "Number"
-
     if type(value) == "boolean" then
-
         prefix = "Bool"
-
         if value then
-
             value = "true"
-
         else
-
             value = "false"
-
         end
-
     elseif type(value) == "string" then
-
         prefix = "String"
         value = '"' .. value .. '"'
-
     end
-
     ResultString = ResultString .. "   " .. prefix .. "ConstantTest(finale." .. v .. ", " .. '"' .. v .. '", ' .. value .. ")\n"
-
 end
 ResultString = ResultString .. "end\n\n"
 
 
-
-
-
 ResultString = ResultString .. "-- Test the constants:\nTestConstants_" .. pattern .. "()\n"
 
-
-
 if finenv.UI():TextToClipboard(ResultString) then
-
     finenv.UI():AlertInfo("Code has been copied to the clipboard.", "Test Created")
-
 end
 
 
